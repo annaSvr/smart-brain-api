e9f60d28f7866f4e870372b208b522a44faa8bfe
annaSvr <svyrydenko.anna@hotmail.com>
2020-04-30 21:10:37 +0300

    adding starting project

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1', //localhost
+    user : 'mac',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {res.send(database.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt)) //two ways of syntax signin and register are the same: in signin.js function looks like this (db, bcrypt) => (req, res) {}
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) }) // this is pretty much the same as signin, just two diff ways of syntax preferences
+app.get('/profile/:id', (req,res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+})
